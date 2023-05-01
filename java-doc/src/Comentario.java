453723915cd384b31bd22f9fb04a5fce268c79fc
JasonLuis <jason20luis@gmail.com>
2023-05-01 10:44:18 -0300

    chore: add sample project to generate Java documentation

---
+++ java-doc/src/Comentario.java
@@ -0,0 +1,21 @@
+public class Comentario {
+    // One Line -> Olá, eu sou um comentário em uma única linha
+
+/*
+ * Multi line
+ * Olá,
+ * Eu sou um comentario
+ * que posso ser mais detalhadod
+ * quando necessário
+ */
+
+
+ /** 
+  * Documentation
+ * Estas duas estrelinhas acima
+ * é para identificar que você
+ * pretende elaborar um comentário
+ * a nível de documentação.
+ * Que incrível !!!
+ */
+}
